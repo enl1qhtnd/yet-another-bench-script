cf4eb49549d8a202dbcecfeda20bef839a602547
Nico <88907425+enl1qhtnd@users.noreply.github.com>
2025-09-28 10:06:53 +0200

    Update yabs.sh

--- yabs.sh
+++ yabs.sh
@@ -881,7 +881,7 @@ if [ -z "$SKIP_IPERF" ]; then
 	#   4. location and advertised speed link of the iperf server
 	#   5. network modes supported by the iperf server (IPv4 = IPv4-only, IPv4|IPv6 = IPv4 + IPv6, etc.)
 	IPERF_LOCS=( \
- 		"speedtest.nl.novoserve.com" "5201-5201" "NovoServe" "Amsterdam, NL (100G)" "IPv4|IPv6" \
+ 		"lg.ams-nl.gigahost.no" "9200-9240" "NovoServe" "Amsterdam, NL (100G)" "IPv4|IPv6" \
 		"a205.speedtest.wobcom.de" "5201-5201" "WOBCOM" "Frankfurt, DE (50G)" "IPv4|IPv6" \
     	"ping.online.net" "5201-5209" "Scaleway" "Paris, FR (100G)" "IPv4" \
     	"ping6.online.net" "5201-5209" "Scaleway" "Paris, FR (100G)" "IPv6" \
